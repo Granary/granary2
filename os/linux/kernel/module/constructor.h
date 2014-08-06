b590ab5564708741615bf548339b92b11d94ea47
pgoodman <peter.goodman@gmail.com>
2014-08-06 17:58:22 -0400

    Got constructors workings as well as a very simple command listener.

--- os/linux/kernel/module/constructor.h
+++
@@ -1,8 +0,0 @@
-/* Copyright 2014 Peter Goodman, all rights reserved. */
-
-#ifndef OS_LINUX_KERNEL_MODULE_CONSTRUCTOR_H_
-#define OS_LINUX_KERNEL_MODULE_CONSTRUCTOR_H_
-
-void RunConstructors(void);
-
-#endif  // OS_LINUX_KERNEL_MODULE_CONSTRUCTOR_H_
